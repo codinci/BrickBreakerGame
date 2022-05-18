218f7f3e75d589e4952a2e05913edb0e4a498338
codinci <dkibau@gmail.com>
2022-05-18 19:52:58 +0300

    Pausing the game

--- game.js
+++ game.js
@@ -4,6 +4,12 @@ import Ball from './ball.js';
 import Brick from './brick.js';
 import { buildLevel, level1 } from './level.js';
 
+const GAMESTATE = {
+    PAUSED: 0,
+    RUNNING: 1,
+    MENU: 2,
+    GAMEOVER: 3
+};
 
 
 export default class Game {
@@ -13,6 +19,7 @@ export default class Game {
     }
 
     start() {
+        this.gameState = GAMESTATE.RUNNING;
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
 
@@ -21,10 +28,11 @@ export default class Game {
 
         this.gameObjects = [this.ball, this.paddle, ...bricks];
 
-        new InputHandler(this.paddle);   
+        new InputHandler(this.paddle, this);   
     }
 
     update(deltaTime) {
+        if(this.gameState == GAMESTATE.PAUSED) return;
         this.gameObjects.forEach(object => object.update(deltaTime));
 
         this.gameObjects = this.gameObjects.filter(object => !object.markedForDeletion);
@@ -32,5 +40,25 @@ export default class Game {
 
     draw(ctx) {
         this.gameObjects.forEach(object => object.draw(ctx));
+
+        if(this.gameState == GAMESTATE.PAUSED) {
+            ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+            ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+            ctx.fill();
+
+            ctx.font = "30px Arial";
+            ctx.fillStyle = "white";
+            ctx.textAlign ="center";
+            ctx.fillText("Paused", this.gameWidth / 2, this.gameHeight / 2);
+        }
+    }
+
+    togglePause() {
+        if(this.gameState == GAMESTATE.PAUSED){
+            this.gameState = GAMESTATE.RUNNING;
+        } else {
+            this.gameState = GAMESTATE.PAUSED;
+        }
+
     }
 }
