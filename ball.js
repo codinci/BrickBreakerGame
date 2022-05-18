bc9bb3bd536529f45d7ccd262c203987f448c333
codinci <dkibau@gmail.com>
2022-05-18 17:56:52 +0300

    Collision of the ball and the paddle

--- ball.js
+++ ball.js
@@ -5,6 +5,8 @@ export default class ball {
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
 
+        this.game = game;
+
         this.position = { x: 10, y: 10};
         this.speed = { x: 2, y: 2};
         this.size = 15;
@@ -15,15 +17,32 @@ export default class ball {
     }
 
     update(deltaTime) {
+        
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
 
+        // checks collision of the ball on the right and left side of the wall
         if(this.position.x + this.size > this.gameWidth || this.position.x < 0) {
             this.speed.x =- this.speed.x;
         }
 
+        // checks collision of the ball on the top and bottom
         if(this.position.y + this.size > this.gameHeight || this.position.y < 0) {
             this.speed.y =- this.speed.y;
+        }
+
+        // checks collision of the ball and the paddle
+        let bottomOfBall = this.position.y + this.size;
+        let topOfPaddle = this.game.paddle.position.y;
+
+        let leftSideOfPaddle = this.game.paddle.position.x;
+        let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+        if(bottomOfBall >= topOfPaddle 
+            && this.position.x >= leftSideOfPaddle
+            && this.position.x + this.size <= rightSideOfPaddle) {
+            this.speed.y =- this.speed.y;
+            this.position.y = this.game.paddle.position.y - this.size;
         }
 
     }
